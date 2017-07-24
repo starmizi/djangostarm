1eb5979d061ef9ca8f12bdd7403f6f99164f714e
starmizi <said.tarmizi@outlook.com>
2017-07-24 14:26:23 +0700

    Add templates

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
-
+    context_dict = {'boldmessage': "Crunchy!"}
+    return render(request, 'rango/index.html', context=context_dict)
